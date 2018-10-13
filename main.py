00fca8e8ef1354eb0af3137b0aab73139985f259
Arpit Garg <iarpitgarg@gmail.com>
2018-10-12 18:35:44 -0600

    variance reduction added; split added

--- main.py
+++ main.py
@@ -6,58 +6,44 @@ Created on Sun Sep 30 20:57:32 2018
 @author: Arpit
 """
 
-from utils import load_data, split_data, plot, save_preds
+from utils import load_data, plot, save_preds
 from naiveBayes import NaiveBayes
 import math
 
 raw_data = load_data('training.csv')
-
 """
 # Trains Naive Bayes on whole training data
 # and makes the prediction on testing data for
 # a chosen value of Beta.
 """
-NB = NaiveBayes(raw_data)
+NB = NaiveBayes(raw_data, var_red=True)
 
-lgx = -2.0
+lgx = -1
 beta = math.pow(10, lgx)
-NB.set_beta(beta)
-NB.train()
+NB.train(beta)
 
-raw_testing_data = load_data('testing.csv')
-rows = raw_testing_data[:, 1:].toarray() #removes 1st column
-preds = list(NB.predict(rows))
+preds = list(NB.predict(NB.preprocess_testing_data(load_data('testing.csv'))))
 save_preds(preds, "prediction.csv")
 
 """
 # Trains Naive Bayes on 85% of the training data.
 # Rest of the data is held for validation.
 # Trains for different values of Beta and plots
-# accuracy vs log(beta).
+# accuracy on validation set vs log(beta).
 """
-raw_training_data, raw_validation_data = split_data(raw_data, 0.85)
-
 #Training on training_data set
-NB = NaiveBayes(raw_training_data)
-
-valid_rows = raw_validation_data[:, 1:-1].toarray()
-valid_labels = list(raw_validation_data[:, -1].toarray()[:, 0])
+NB = NaiveBayes(raw_data, split=0.85, var_red=True)
 
 Xs = [] # X coordinates for the graph plot
 Ys = [] # Y coordinates for the graph plot
 lgx = -5
 while lgx <= 0:
     beta = math.pow(10, lgx)
-    NB.set_beta(beta)
-    NB.train()
+    NB.train(beta)
+    accuracy = NB.check_accuracy() #Checking accuracy on validation set
     
-    #Predicting on validation set to check accuracy
-    preds = list(NB.predict(valid_rows))
-    accuracy = sum([1 if valid_labels[i]==preds[i] else 0 for i in range(len(valid_labels))])/len(valid_labels)
     Xs.append(lgx)
     Ys.append(accuracy)
-    print(lgx, accuracy)
-    
     lgx += .05
     
 plot(Xs, Ys)
--- naiveBayes.py
+++ naiveBayes.py
@@ -8,9 +8,15 @@ Created on Mon Oct  1 18:04:51 2018
 import numpy as np
 from scipy import sparse
 from collections import Counter
+from sklearn.feature_selection import VarianceThreshold
+from utils import split_data
 
 class NaiveBayes:
-    def __init__(self, data):
+    def __init__(self, data, split=1.0, var_red=True):
+        self.split = split
+        self.var_red = var_red
+        self.sel = VarianceThreshold()
+
         self.preprocess_data(data)
         self.vocab_len = self.xi_cnt_in_yk.shape[1]
         self.set_beta(1/self.vocab_len)
@@ -19,19 +25,32 @@ class NaiveBayes:
     Computes a matrix for P(Xi/Yk)
     and another matrix for P(Yk)
     """
-    def train(self):
+    def train(self, beta=None):
         print("Training")
+        if beta is not None: self.set_beta(beta)
+        
         total_words_in_yk = self.xi_cnt_in_yk.sum(1, keepdims=True) #shape (20, 1)
         denominator = total_words_in_yk + (self.alpha - 1) * self.vocab_len #shape (20, 1)
         
         self.prob_xi_given_yk = (self.xi_cnt_in_yk + (self.alpha - 1)) / denominator #shape (20, vocab_len)
         self.prob_yk = self.label_dist / sum(self.label_dist)
+        
+        if self.split < 1:
+            self.check_accuracy()
 
     def predict(self, rows):
         print("Predicting")
         prob_yk_given_document = np.dot(np.log2(self.prob_xi_given_yk), rows.T) + np.log2(self.prob_yk)
         preds = np.argmax(prob_yk_given_document, 0) + 1
         return preds
+    
+    def check_accuracy(self):
+        preds = self.predict(self.X_valid)
+        labels = self.Y_valid[:, 0]
+        accuracy = sum([1 if labels[i]==preds[i] else 0 for i in range(len(labels))])/len(labels)
+        print("Validation Set Accuracy:", accuracy)
+        
+        return accuracy
     
     """
     Calculates the number of times a word has
@@ -41,21 +60,38 @@ class NaiveBayes:
     """
     def preprocess_data(self, data):
         print("Pre-processing data")
-        labels = data[:, -1].toarray()
-        data = data[:, 1:-1]
+        
+        #Splits the data into training and validation sets
+        X, X_valid = split_data(data, self.split)
+        
+        Y = X[:, -1].toarray()
+        X = X[:, 1:-1]
+        self.Y_valid = X_valid[:, -1].toarray()
+        X_valid = X_valid[:, 1:-1]
+        
+        if self.var_red:
+            X = self.sel.fit_transform(X)
+            if X_valid.shape[0] > 0: X_valid = self.sel.transform(X_valid)
+            
+        self.X_valid = X_valid.toarray()
         
         #separates the data class wise
         data_list = []
-        for label in np.unique(labels):
-            rows = np.where(labels == label)[0]
-            data_list.append(data[rows].sum(0).tolist()[0])
+        for label in np.unique(Y):
+            rows = np.where(Y == label)[0]
+            data_list.append(X[rows].sum(0).tolist()[0])
         
-        label_dist = Counter(list(labels.T[0]))
+        label_dist = Counter(list(Y.T[0]))
         label_dist = np.array(sorted(label_dist.items()))[:, 1]
         label_dist = label_dist.reshape(len(label_dist), 1)
         
         self.xi_cnt_in_yk = sparse.csr_matrix(data_list).toarray()
         self.label_dist = label_dist
+    
+    def preprocess_testing_data(self, data):
+        data = data[:, 1:].toarray() #removes 1st column
+        if self.var_red: data = self.sel.transform(data)
+        return data
         
     def set_beta(self, beta):
         self.beta = beta
